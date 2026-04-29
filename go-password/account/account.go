77a8546591d376222e83e5f0d0286970941a177b
Ungravity <ungravity@MacBook-Pro-Ungravity.local>
2026-04-29 20:16:23 +0300

    update password generator

---
+++ go-password/account/account.go
@@ -0,0 +1,59 @@
+package Account 
+
+import (
+	"errors"
+	"fmt"
+	"math/rand/v2"
+	"net/url"
+	"time"
+)
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@!~*$#%^&")
+
+type Account struct {
+	login string 
+	password string 
+	url string
+}
+
+type AccountWithTimeStamp struct {
+	createdAt time.Time
+	updatedAt time.Time
+	Account
+}
+
+func (acc *AccountWithTimeStamp) OutputPassword() {
+	fmt.Println(acc.login, acc.password, acc.url, acc.createdAt, acc.updatedAt)
+}
+
+func (acc *Account) generatePassword(n int) {
+	res := make([]rune, n)
+	for i := range res {
+		res[i] = letterRunes[rand.IntN(len(letterRunes))]
+	}
+	acc.password = string(res)
+}
+
+func NewAccountWithTimeStamp(login, password, urlString string) (*AccountWithTimeStamp, error) {
+	if login == "" {
+		return nil, errors.New("INVALID_LOGIN!")
+	}
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, errors.New("INVALID_URL!")
+	}
+	newAcc := &AccountWithTimeStamp{
+		Account: Account {
+			login: login,
+			password: password,
+			url: urlString,
+		},
+		createdAt: time.Now(),
+		updatedAt: time.Now(),
+	}
+	if password == "" {
+		newAcc.generatePassword(12)
+	}
+	return newAcc, nil
+}
+
--- go-password/main.go
+++ go-password/main.go
@@ -1,63 +1,22 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"math/rand/v2"
-	"net/url"
+	"soda/password/account"
 )
-
-type account struct {
-	login string 
-	password string 
-	url string
-}
-
-func (acc *account) outputPassword() {
-	fmt.Println(acc.login, acc.password, acc.url)
-}
-
-func (acc *account) generatePassword(n int) {
-	res := make([]rune, n)
-	for i := range res {
-		res[i] = letterRunes[rand.IntN(len(letterRunes))]
-	}
-	acc.password = string(res)
-}
-
-func newAccount(login, password, urlString string) (*account, error) {
-	if login == "" {
-		return nil, errors.New("INVALID_LOGIN!")
-	}
-	_, err := url.ParseRequestURI(urlString)
-	if err != nil {
-		return nil, errors.New("INVALID_URL!")
-	}
-	newAcc := &account{
-		login: login,
-		password: password,
-		url: urlString,
-	}
-	if password == "" {
-		newAcc.generatePassword(12)
-	}
-	return newAcc, nil
-}
-
-var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@!~*$#%^&")
 
 func main() {
 	login := promptData("Enter Your Login")
 	password := promptData("Enter Your Password")
 	url := promptData("Enter URL")
 
-	myAccount, err := newAccount(login, password, url)
+	myAccount, err := Account.NewAccountWithTimeStamp(login, password, url)
 	if err != nil {
 		fmt.Println("Invalid LOGIN or URL! Check Your Input.")
 		return
 	}
 	
-	myAccount.outputPassword()
+	myAccount.OutputPassword()
 }
 
 func promptData(prompt string) string {
